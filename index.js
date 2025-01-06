67b27e55445b0ace1588c42c62d3adea65804fc8
hari228282 <hari228282@gmail.com>
2025-01-06 10:58:17 +0530

    some another code send

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express') //todo common js module
+// import express from 'express'; //todo ES6 Module 2015
 require('dotenv').config()
 const app = express()
 const port = 4000
@@ -8,11 +9,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/humesaMangtaHai', (req, res) => {
-    res.send("Haa Bhai Hariom kya haal hia yaar mujhe na kuchh paise ki jarurat hai yaar kuchh bhej de na bhai")
+    res.send("Haa Bhai Hariom kya haal hai yaar mujhe na kuchh paise ki jarurat hai yaar kuchh bhej de na bhai")
 })
 
 app.get('/HariBhog', (req, res) => {
     res.send("<h1>Welcome to Hariom Bhojnalaya</h1>")
+})
+
+app.get('/anniBabu', (req, res) => {
+    res.send("<li>Aur bhai kya haal chal</li>")
 })
 
 app.listen(process.env.PORT, () => {
